1d8d9ef8f7e1b698e4554d19319863a883def07c
francesco <francescosacco.github@gmail.com>
2022-03-19 11:37:55 +0100

    Cleanening the Text for Better Searching

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ except IndexError:
 
 # Converting thge Image to Text
 text = pytesseract.image_to_string(PIL.Image.open(path), config=CONFIG)
+
+# String Manipulation for Clenaing the Data
+text = text.replace("ORIZZONTALI", "")
+text = text.replace("VERTICALI", "")
+text = text.replace(":", "")
+text = text.replace("-", "")
+text = text.replace("_", "")
+text = text.replace(".", "")
+not_filtered_list = text.split()
+new_text = ""
+for i in range(len(not_filtered_list)):
+    if not_filtered_list[i] not in ["", "__", "_", "\n"]:    
+        if not_filtered_list[i].isdigit() and i != 0  and len(not_filtered_list[i]) not in [3, 4]:
+            new_text += "\n"
+        new_text += f"{not_filtered_list[i]} "
